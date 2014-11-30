2a0f90dbd47a43fc50dcd659cceb68c8b090d854
Shadey <me@shadey.net>
2014-11-29 21:57:40 -0500

    Logging better

---
+++ AnzuBot-master/config.js
@@ -0,0 +1,8 @@
+{
+        "server": "someserver.net",
+        "userName": "Anzu-Bot",
+        "nickName": "Anzu",
+        "realName": "Futaba Anzu",
+        "channel": ["#somechannel"],
+        "admins" : [""]
+}
